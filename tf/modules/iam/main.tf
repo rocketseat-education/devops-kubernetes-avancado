5eb7e48b875dbb42de9ac4c3e0ad72a05a137246
EgydioBNeto <egydiobolonhezi@gmail.com>
2026-04-13 16:55:11 +0000

    Terraformando nosso primeiro recurso já existente

--- tf/modules/iam/main.tf
+++ tf/modules/iam/main.tf
@@ -66,3 +66,33 @@ resource "aws_iam_role_policy_attachment" "eks_ebs_csi_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.eks_nodes_role.name
 }
+
+resource "aws_iam_role" "karpenter_cluster_role_rocketseat_cluster" {
+  path        = "/"
+  name        = "karpenter-cluster-role-rocketseat-cluster"
+  description = "Role do cluster Karpenter."
+  assume_role_policy = jsondecode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Federated" : "arn:aws:iam::403429280851:oidc-provider/oidc.eks.us-east-2.amazonaws.com/id/E91BF7D91336C814A0E2CBC5488FF48D"
+          },
+          "Action" : "sts:AssumeRoleWithWebIdentity",
+          "Condition" : {
+            "StringEquals" : {
+              "oidc.eks.us-east-2.amazonaws.com/id/E91BF7D91336C814A0E2CBC5488FF48D:aud" : "sts.amazonaws.com",
+              "oidc.eks.us-east-2.amazonaws.com/id/E91BF7D91336C814A0E2CBC5488FF48D:sub" : "system:serviceaccount:kube-system:karpenter"
+            }
+          }
+        }
+      ]
+    }
+  )
+  max_session_duration = 3600
+  tags = {
+    IAC = "True"
+  }
+}
